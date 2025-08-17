7b3e992cf5cb47a9511c7099a6d1dff0c2e2c6d0
Alireza Souri <aalalirezasouri@gmail.com>
2025-08-17 17:18:18 +0330

    commit

--- src/models/main.py
+++ src/models/main.py
@@ -15,13 +15,19 @@ logger = CustomLogger(name="model_main", log_file_name='model_main.log').get_log
 if __name__ == "__main__":
     csv_path = os.path.join(project_root, "data", "processed", "integrated.csv")
     df = pd.read_csv(csv_path, encoding='utf-8')
+    logger.info(f"Csv file has bean read successfully")
 
-    data_selector = Data_selector(df)
-    data_selector.create_delay("temperature", 1)
-    data_selector.create_delay("temperature", 2)
-    data_selector.create_delay("temperature", 3)
-    df_modified = data_selector.select()
+    feature_adder = Feature_adder(df)
+    feature_adder.add_season()
+    feature_adder.create_feature_with_delay("temperature", 1)
+    feature_adder.create_feature_with_delay("temperature", 2)
+    feature_adder.create_feature_with_delay("temperature", 3)
+    logger.info(f"Some features have been added successfully")
 
+    data_selector = Data_selector(feature_adder.df)
+    df_modified = data_selector.select(m_in_summer=True)
+    logger.info(f"Rows have been selected successfully")
+    
     feature_selector = Feature_selector(df_modified, "generation")
     # feature_to_be_dropped = ['id', 'hour', 'date', 'status', 'declare']
     feature_to_be_dropped = ['id', 'date', 'declare', 'dew', 'apparent_temperature', 'rain', 'snow',
@@ -41,7 +47,7 @@ if __name__ == "__main__":
     # model.scale_and_split_data(X, y)
     # model.fit()
 
-    model = Random_Forest()
+    logger.info(f"Model has been trained successfully")
     # model = Neural_network()
     model = Random_Forest()
     #model = Linear()
