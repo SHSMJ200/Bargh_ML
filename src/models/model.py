3f02c7973e1642f43390342bc1b903fee07a2dc4
Alireza Souri <aalalirezasouri@gmail.com>
2025-09-25 16:28:09 +0330

    transfer some files

---
+++ src/models/model.py
@@ -0,0 +1,18 @@
+class Model():
+    
+    model_path = ""
+    
+    def __init__(self):
+        pass
+    
+    def load(self):
+        pass
+    
+    def save(self):
+        pass
+    
+    def fit(self):
+        return
+    
+    def predict(self):
+        return
