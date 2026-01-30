829baad38f6e9271b84dad4eed898c666b65f176
Alireza Souri <aalalirezasouri@gmail.com>
2026-01-30 08:49:16 +0330

    commited

--- src/data/main.py
+++ src/data/main.py
@@ -5,15 +5,11 @@ current_dir = os.path.dirname(os.path.abspath(__file__))
 project_root = current_dir[:current_dir.find("src") - 1]
 sys.path.insert(0, project_root)
 
-<<<<<<< HEAD
 from src.data.aggregation import integrated_aggregation
 from src.data.data_cleaner import process_all_csv_files
 
 
 
-=======
-
->>>>>>> 164075d33a64a17fab7647ce332420262a563800
 def preprocess_data():
     process_all_csv_files()
     integrated_aggregation()
